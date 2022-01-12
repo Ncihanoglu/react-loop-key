ccbef9fa63f1a32414ce23d3a6623287c10145a4
Ncihanoglu <nihatcihanoglu@Nihat-MacBook-Pro.local>
2022-01-12 18:10:52 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [list, setList] = useState([{ id: 1, value: Math.random() }]);
+
+  const addNewElementToStart = () => {
+    const tempArray = [...list];
+    const willaddElement = { id: generateUniqueId(), value: Math.random() };
+    tempArray.unshift(willaddElement);
+    setList(tempArray);
+  };
+
+  const generateUniqueId = () => {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
+      /[xy]/g,
+      function (c) {
+        var r = (Math.random() * 16) | 0,
+          v = c === 'x' ? r : (r & 0x3) | 0x8;
+        return v.toString(16);
+      }
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header" style={{ alignItems: 'baseline' }}>
+      <div>key equals index</div>
+      {list.map((item, index) => (
+        <div key={index}>
+          <input style={{ marginTop: '10px' }} />
+          <label style={{ marginLeft: '10px' }}>{item.value}</label>
+        </div>
+      ))}
+      <hr style={{ width: '100%' }} />
+      <div>key equals uniqueId </div>
+      {list.map((item) => (
+        <div key={item.id}>
+          <input style={{ marginTop: '10px' }} />
+          <label style={{ marginLeft: '10px' }}>{item.value}</label>
+        </div>
+      ))}
+      <hr style={{ width: '100%' }} />
+      <button onClick={addNewElementToStart}> add New Element to start</button>
     </div>
   );
 }
